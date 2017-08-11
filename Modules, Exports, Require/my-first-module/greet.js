19c738d4a32fdae2b06bcc9c1ab015a5e62eabc9
Dan Austin <danieljohnaustin@gmail.com>
2017-08-11 15:54:33 +0100

    more explanation into the way require works with module.exports

--- Modules, Exports, Require/my-first-module/greet.js
+++ Modules, Exports, Require/my-first-module/greet.js
@@ -7,6 +7,12 @@ const greet = function(){
 
 // If we do want to make members available to another file (app.js) we use the following line:
 module.exports = greet;
+
+// the module object is available here because the node engine wraps the code within a function expression.
+// Among other things, this function expression takes and is provided with the 'module' object in which to set the 'exports' value.
+// Once the function wrapper is executed, and the module object has been updated by our code, the value of module.exports is returned by the require function
+
+
 // This module.exports object is returned when require(...) is called on this file
 // This allows us to choose what will be available to other files outside of our module. Only the things attached to this variable  will be available outside the module for use.
 // We can attach an object instead of just a function, this allows us to export a lot of functionality:
