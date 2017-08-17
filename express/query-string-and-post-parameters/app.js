3cd278b7f64eb040f3b704c0671f30e3aa7e4be9
Dan Austin <danieljohnaustin@gmail.com>
2017-08-17 11:04:16 +0100

    Add bad explanation comment

--- express/query-string-and-post-parameters/app.js
+++ express/query-string-and-post-parameters/app.js
@@ -9,8 +9,12 @@ const jsonParser = bodyParser.json();
 app.set('view engine', 'pug');
 app.use('/assets', express.static('./public'));
 
+// The following require statements return functions that take express apps as parameters
+// The when the functions are called, a series of app.get, app.post, app.delete etc. calls are run to add the endpoints to this app 
+
 // add api controllers to this app
 require('./controllers/api.js')(app);
+// add html controllers to this app
 require('./controllers/html.js')(app);
 
 app.listen(port, () => {
